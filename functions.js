8204af7a9d5e33a0cbc8ebee7ebe83a7b79e5c31
Michael Chan <michael.w.chan01@gmail.com>
2024-05-28 23:16:58 -0400

    Part 2

--- functions.js
+++ functions.js
@@ -53,4 +53,38 @@ let strings = ['food', 'drink', 'chicken'];
 // console.log(average(array));
 // console.log(longest_string(strings));
 // console.log(stringsLongerThan(strings, 5));
-printNumbers(5);
+// printNumbers(5);
+
+//Part 2
+let people = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+{ id: "48", name: "Barry", occupation: "Runner", age: "25" },
+{ id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+{ id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+{ id: "7", name: "Bilbo", occupation: "None", age: "111" }];
+
+//Sort Array by age
+people.sort((a,b) => a.age - b.age);
+// console.log(people);
+
+//Filter array to remove entries with an age greater than 50
+younger_than_50 = people.filter(person => person.age > 50);
+// console.log(younger_than_50);
+
+//Map the array to change occupation key to job and increment every age by 1.
+let mapped = people.map(person =>{
+    return{
+        id: person.id,
+        name: person.name,
+        job: person.occupation,
+        age: String(Number(person.age) + 1)
+    };
+});
+// console.log(mapped);
+
+//Use reduce method to calculate sum of ages
+let totalAge = people.reduce((accumulator, item) => {
+    return accumulator += Number(item.age);
+}, 0)
+console.log(totalAge)
+average_age = totalAge / people.length;
+console.log(average_age);
